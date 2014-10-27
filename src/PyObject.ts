45cb6e900f3b07a3fd16a66d291a5208a0a11752
r3d3y3 <ppegusii@gmail.com>
2014-10-27 17:11:47 -0400

    BINARY_ADD working

--- src/PyObject.ts
+++ src/PyObject.ts
@@ -3,13 +3,6 @@ import Exceptions = require('./Exceptions');
 
 class PyObject{
   private type: enums.PyType;
-  //private value: any;
-  /*
-  constructor(type: enums.PyType, value: any){
-    this.type = type;
-    this.value = value;
-  }
-  */
   constructor(type: enums.PyType){
     this.type = type;
   }
